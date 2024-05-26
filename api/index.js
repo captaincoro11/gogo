a99056d116ef5e32f5b4de56929470f0511a03ed
captaincoro11 <captaincoro444@gmail.com>
2024-05-26 15:16:08 +0530

    first commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const { connectDatabase } = require('../db');
+const userRoutes = require('../routes/user');
+const taskRoutes = require('../routes/task');
+const cors = require('cors');
+const bodyParser = require('body-parser')
+
+
+dotenv.config({});
+
+connectDatabase();
+app.use(cors());
+app.use(express.json({limit:"50mb"}))
+app.use(express.static('public'))
+app.use('/user',userRoutes)
+app.use('/task',taskRoutes)
+
+
+const port = process.env.port || 5000;
+
+app.get('/',(req,res)=>{
+    res.json("HELLO BABY")
+})
+
+
+
+
+app.listen(port,()=>{
+    console.log(`It started on ${port}`)
+})
